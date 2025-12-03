e72e1670469b2bbd4adb3ef0f3081aacbec7cb2d
Climbingsyl <climbingsyl@hotmail.com>
2025-12-03 17:17:25 +0100

    Add files via upload

---
+++ climbing_syl_project/app/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = { title: "Climbing Syl" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ climbing_syl_project/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Climbing Syl – Site en construction</h1>;
+}
